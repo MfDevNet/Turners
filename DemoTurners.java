2ed7e1909be452476858f33f70fedede7d752d6f
msghf1366 <msghf1366@gmail.com>
2019-04-16 19:43:57 +0430

    create class DemoTurners and use class leaf,page,pancake

---
+++ DemoTurners.java
@@ -0,0 +1,14 @@
+package demoturners;
+
+public class DemoTurners {
+    public static void main(String[] args) {
+        Leaf leaf = new Leaf();
+        Page page = new Page();
+        Pancake cake = new Pancake();
+        leaf.turn();
+        page.turn();
+        cake.turn();
+    }
+
+
+}
